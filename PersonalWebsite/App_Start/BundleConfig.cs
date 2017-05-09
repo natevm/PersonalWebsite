231cc06ed421c91de8d5a49ec1cee8613bc1931b
Nathan Vollmer <bitinat2@isu.edu>
2017-05-08 20:55:44 -0600

    Updated content

--- PersonalWebsite/App_Start/BundleConfig.cs
+++ PersonalWebsite/App_Start/BundleConfig.cs
@@ -8,24 +8,22 @@ namespace PersonalWebsite
 		// For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
 		public static void RegisterBundles(BundleCollection bundles)
 		{
+			/* jquery */
 			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-									"~/Scripts/jquery-{version}.js"));
+			          "~/Scripts/jquery/jquery-{version}.js"));
 
-			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-									"~/Scripts/jquery.validate*"));
-
-			// Use the development version of Modernizr to develop with and learn from. Then, when you're
-			// ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-									"~/Scripts/modernizr-*"));
-
-			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-								"~/Scripts/bootstrap.js",
-								"~/Scripts/respond.js"));
+			/* angularjs */
+			bundles.Add(new ScriptBundle("~/bundles/angularjs").Include(
+								"~/Scripts/angular/angular.min.js",
+								"~/Scripts/angular-animate/angular-animate.min.js",
+								"~/Scripts/angular-aria/angular-aria.min.js",
+								"~/Scripts/angular-messages/angular-messages.min.js",
+								"~/Scripts/angular-material/angular-material.min.js"));
 
 			bundles.Add(new StyleBundle("~/Content/css").Include(
-								"~/Content/bootstrap.css",
-								"~/Content/site.css"));
+								"~/Content/angular-material/angular-material.min.css",
+								"~/Content/main.css",
+								"~/Content/Sections/About Me/AboutMe.css"));
 		}
 	}
 }
